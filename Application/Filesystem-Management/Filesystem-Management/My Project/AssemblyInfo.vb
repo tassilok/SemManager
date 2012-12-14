04334ba8246537c474a8f4f9d562b4dcbf89973c
Tassilo Koller <tassilok@gmx.de>
2012-12-14 22:47:39 +0100

    New Version of Filesystem Module

--- Application/Filesystem-Management/Filesystem-Management/My Project/AssemblyInfo.vb
+++ Application/Filesystem-Management/Filesystem-Management/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.0.80")> 
-<Assembly: AssemblyFileVersion("0.1.0.80")> 
+<Assembly: AssemblyVersion("0.1.0.81")> 
+<Assembly: AssemblyFileVersion("0.1.0.81")> 
