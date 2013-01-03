8486c4e58ba76e291473a57b7bfd9028c35611b2
Tassilo Koller <tassilok@gmx.de>
2013-01-03 22:56:07 +0100

    Hash of Files

--- Application/Filesystem-Management/Filesystem-Management/My Project/AssemblyInfo.vb
+++ Application/Filesystem-Management/Filesystem-Management/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.0.81")> 
-<Assembly: AssemblyFileVersion("0.1.0.81")> 
+<Assembly: AssemblyVersion("0.1.0.82")> 
+<Assembly: AssemblyFileVersion("0.1.0.82")> 
